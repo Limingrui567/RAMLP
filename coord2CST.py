fd53b7a00b24a2cb5a43e968ba0bdc981ae7527a
Lucky-Lee-codemaker <981530298@qq.com>
2025-06-13 00:16:18 +0800

    Update coord2CST.py

--- coord2CST.py
+++ coord2CST.py
@@ -12,7 +12,7 @@ def coord_2_CST(coord, dzu, dzl, order=6):
         f = 0
         for i in range(len(y2)):
             f += (coord[i, 1] * 100 - y2[i] * 100) ** 2
-            #f是目标函数
+            # f is the objective function
         return f
     res = optimize.minimize(function_CST,  CST_parameter_0, args=(dzu, dzl), method='SLSQP',
                             options={'ftol': 1e-16, 'disp': False},
